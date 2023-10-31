959c8f3f994bda04b3627066e4c974043c4f5c71
Tientd0111 <tientd0111@gmail.com>
2023-10-31 16:24:14 +0700

    u

--- src/routes/checkAuth.jsx
+++
@@ -1,8 +0,0 @@
-export const checkAuth = () => {
-   const user = JSON.parse(localStorage.getItem('user'))
-   if (user?.email === undefined) {
-      return false
-   } else {
-      return true
-   }
-}
