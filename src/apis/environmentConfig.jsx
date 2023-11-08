fe6d7a4931a14d3f2fa1b5ff8480b8ad71e8bbf0
Tientd0111 <tientd0111@gmail.com>
2023-11-08 17:17:32 +0700

    u

--- src/apis/environmentConfig.jsx
+++ src/apis/environmentConfig.jsx
@@ -1,13 +1,13 @@
 let ConfigApi;
 // eslint-disable-next-line no-unused-vars
 export default ConfigApi = {
-   // API_ENVIRONMENT_URL: 'http://localhost:8080/api/v1/',
-   // API_UPLOAD: 'http://localhost:8080/',
-   // BASE_URI: "http://localhost:8080/upload/",
-   // URI_SOCKET: "http://localhost:8080",
-   API_ENVIRONMENT_URL: 'https://chat-q7jd.onrender.com/api/v1/',
-   API_UPLOAD: 'https://chat-q7jd.onrender.com/',
-   BASE_URI: "https://chat-q7jd.onrender.com/upload/",
-   URI_SOCKET: "https://chat-q7jd.onrender.com",
+   API_ENVIRONMENT_URL: 'http://localhost:8080/api/v1/',
+   API_UPLOAD: 'http://localhost:8080/',
+   BASE_URI: "http://localhost:8080/upload/",
+   URI_SOCKET: "http://localhost:8080",
+   // API_ENVIRONMENT_URL: 'https://chat-q7jd.onrender.com/api/v1/',
+   // API_UPLOAD: 'https://chat-q7jd.onrender.com/',
+   // BASE_URI: "https://chat-q7jd.onrender.com/upload/",
+   // URI_SOCKET: "https://chat-q7jd.onrender.com",
    TIME_OUT: 6000,
 }
